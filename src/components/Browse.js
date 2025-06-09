2dd58541e13b3ffb13bfc0d2c82e39475ababa42
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-09 23:25:19 +0530

    Fetch data

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { useEffect } from 'react'
 import Header from "./Header";
+import { API_OPTION } from "../utils/constant";
 
 const Browse = () => {
+
+  const getMovieApi = async () => {
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=00bfff38e3de2c230d04083147e9339c';
+    const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(url);
+    const response = await fetch(proxyUrl);
+    const data = await response.json();
+    const json = JSON.parse(data.contents)
+    console.log(json); // allorigins wraps the response in a 'contents' field
+  }
+  useEffect(() => {
+    getMovieApi();
+  }, []);
+
   return (
     <div>
-      <Header/>
+      <Header />
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,6 @@ import Header from './Header';
 import { checkValidateData } from "../utils/validate";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
@@ -11,7 +10,6 @@ const Login = () => {
 
     const [IsSignIn, setIsSignIn] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
-    const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const email = useRef(null);
@@ -36,7 +34,6 @@ const Login = () => {
                     }).then(() => {
                         const { uid, email, displayName, photoURL } = auth.currentUser;
                         dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
-                        navigate("/browse");
                     }).catch((error) => {
                         setErrorMessage(errorMessage)
                     });
@@ -52,8 +49,6 @@ const Login = () => {
             signInWithEmailAndPassword(auth, email.current.value, password.current.value)
                 .then((userCredential) => {
                     const user = userCredential.user;
-                    // console.log(user);
-                    navigate("/browse");
                 })
                 .catch((error) => {
                     const errorCode = error.code;
