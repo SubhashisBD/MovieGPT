c15a8016b15a549a3975fc2d81334e750270b3aa
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-09 23:24:45 +0530

    Added const

---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const API_OPTION = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGJmZmYzOGUzZGUyYzIzMGQwNDA4MzE0N2U5MzM5YyIsIm5iZiI6MTc0OTQzOTE1MS4zMTQsInN1YiI6IjY4NDY1MmFmZDY2OTIzNWRlMGJiMmY1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eTewD7H6PjK9ASeq95foxowJrAuJWP76kE1ZJYGPvqA',
+  }
+}
+
+export const API_KEY = "00bfff38e3de2c230d04083147e9339c"; // Your v3 API Key
