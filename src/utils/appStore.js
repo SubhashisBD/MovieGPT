f41439077f319ba88b5a144b74304b0fe27c65f5
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-10 20:50:06 +0530

    Added movie slice

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "./userSlice";
+import moviesReducer from "./moviesSlice";
 
 const appStore = configureStore(
     {
         // Different reducers from different slices
         reducer: {
             user: UserReducer,
+            movies: moviesReducer,
+            
         },
     }
 );
