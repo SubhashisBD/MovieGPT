92aafb0c457bc8325c1650a0442f122152595fb2
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-10 20:50:31 +0530

    Hooks for modularity

---
+++ src/hooks/useGetMoviesVideo.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
+// Fetch movies Trailer
+
+const useGetMoviesVideo = (movieId) => {
+
+    const dispatch = useDispatch();
+
+    const getMovievideo = async () => {
+        const url = 'https://api.themoviedb.org/3/movie/' + movieId + '/videos?language=en-US&api_key=00bfff38e3de2c230d04083147e9339c';
+        const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(url);
+        const response = await fetch(proxyUrl);
+        const data = await response.json();
+        const json = JSON.parse(data.contents);
+        console.log(json);
+        // console.log(json);
+        const filterMovieData = json.results.filter(movie=>movie.type==="Trailer");
+        const trailer = filterMovieData[0];
+        console.log(trailer.key);
+        dispatch(addTrailerVideo(trailer));
+    }
+
+    useEffect(() => {
+        getMovievideo();
+    }, []);
+
+}
+
+
+export default useGetMoviesVideo;
+
---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,28 @@
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+// Fetch Data From TMDB API and Update the Store
+
+
+const useNowPlayingMovies = () => {
+
+    const dispatch = useDispatch();
+
+    const getMovieApi = async () => {
+        const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=00bfff38e3de2c230d04083147e9339c';
+        const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(url);
+        const response = await fetch(proxyUrl);
+        const data = await response.json();
+        const json = JSON.parse(data.contents)
+        // AllOrigins proxy wraps the API result in a `contents` field
+        // console.log(json.results);
+        dispatch(addNowPlayingMovies(json.results));
+
+    }
+    useEffect(() => {
+        getMovieApi();
+    }, []);
+}
+
+export default useNowPlayingMovies;
