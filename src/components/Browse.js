e75b8153df60ae34160ae5646e42ddfa623a9b47
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-10 20:45:41 +0530

    Api to fetch movie

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,24 +1,18 @@
-import { useEffect } from 'react'
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { API_OPTION } from "../utils/constant";
+import MainContainer from "./Maincontainer";
+import SecondaryContainer from "./SecondaryContainer";
+
 
 const Browse = () => {
 
-  const getMovieApi = async () => {
-    const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=00bfff38e3de2c230d04083147e9339c';
-    const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(url);
-    const response = await fetch(proxyUrl);
-    const data = await response.json();
-    const json = JSON.parse(data.contents)
-    console.log(json); // allorigins wraps the response in a 'contents' field
-  }
-  useEffect(() => {
-    getMovieApi();
-  }, []);
+ useNowPlayingMovies();
 
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   )
 }
