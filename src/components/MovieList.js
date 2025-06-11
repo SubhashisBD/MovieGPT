67cb6d0a75ec17937dc51416832a196943ec883a
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-11 23:34:52 +0530

    List of different movies

---
+++ src/components/MovieList.js
@@ -0,0 +1,18 @@
+import MovieCard from "./MovieCard"
+
+const MovieList = ({ title, movies }) => {
+  if (!movies) return null;
+  return (
+    <div className="px-4 bg-black">
+      <h1 className="text-white py-4 text-3xl">{title}</h1>
+      <div className="flex overflow-x-scroll scrollbar-none">
+
+        <div className="flex">
+          {movies.map(movies => <MovieCard key={movies.id} posterPath={movies.poster_path} />)}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieList;
