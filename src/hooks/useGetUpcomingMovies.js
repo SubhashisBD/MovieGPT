868c2911d56e37a1dc1aa93f6e664fec7cfd4d33
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-13 23:16:26 +0530

    upcomig movies list

---
+++ src/hooks/useGetUpcomingMovies.js
@@ -0,0 +1,32 @@
+import { useDispatch } from "react-redux";
+import {  addGetUpcomingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+// Fetch Data From TMDB API and Update the Store
+
+
+const useGetUpcomingMovies = () => {
+
+    const dispatch = useDispatch();
+
+    const getMovieApi = async () => {
+        const url = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=00bfff38e3de2c230d04083147e9339c';
+        const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(url);
+        const response = await fetch(proxyUrl);
+        const data = await response.json();
+        const json = JSON.parse(data.contents)
+
+        //* AllOrigins proxy wraps the API result in a `contents` field
+        // *console.log(json.results);
+        console.log("GetTOPRated");
+        console.log(json);
+        
+        dispatch( addGetUpcomingMovies(json.results));
+
+    }
+    useEffect(() => {
+        getMovieApi();
+    }, []);
+}
+
+export default useGetUpcomingMovies ;
