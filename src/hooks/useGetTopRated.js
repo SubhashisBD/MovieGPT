61c085a3380afc131cb09c16a5f28c9f38af7bbf
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-13 23:16:50 +0530

    toprated movies list

---
+++ src/hooks/useGetTopRated.js
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux";
+import {  addGetTopRated } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+// Fetch Data From TMDB API and Update the Store
+
+
+const useGetTopRated = () => {
+
+    const dispatch = useDispatch();
+
+    const getMovieApi = async () => {
+        const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=00bfff38e3de2c230d04083147e9339c';
+        const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(url);
+        const response = await fetch(proxyUrl);
+        const data = await response.json();
+        const json = JSON.parse(data.contents)
+
+        //* AllOrigins proxy wraps the API result in a `contents` field
+        // *console.log(json.results);
+        
+        dispatch( addGetTopRated(json.results));
+
+    }
+    useEffect(() => {
+        getMovieApi();
+    }, []);
+}
+
+export default useGetTopRated;
