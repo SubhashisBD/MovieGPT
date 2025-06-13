c7bf1362ecc9e32e4707b47e4417749d74af4b5f
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-13 23:19:21 +0530

    Added the movie api component

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,17 +2,32 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
 import MainContainer from "./Maincontainer";
 import SecondaryContainer from "./SecondaryContainer";
-
+import useGetPopularMovies from "../hooks/useGetPopularMovies";
+import useGetTopRated from "../hooks/useGetTopRated";
+import useGetUpcomingMovies from "../hooks/useGetUpcomingMovies";
+import { useSelector } from "react-redux";
+import GptSearch from "./GptSearch";
 
 const Browse = () => {
 
-  const nowPlayingMovies = useNowPlayingMovies();
+  const toggleGpt = useSelector(store => store.gpt.gptSearch);
+
+  useNowPlayingMovies();
+  useGetPopularMovies();
+  useGetTopRated();
+  useGetUpcomingMovies();
 
   return (
     <div>
       <Header />
-      <MainContainer />
-      <SecondaryContainer />
+      {
+        toggleGpt ? <GptSearch /> :
+          <>
+            <MainContainer />
+            <SecondaryContainer />
+          </>
+      }
+
     </div>
   )
 }
