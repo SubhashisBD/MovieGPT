4f71e07973e522c87fa7993ec878bdedc0eb7670
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-06-26 08:41:07 +0530

    Added bg Image of gptsearch

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestion from "./GptMovieSuggestion";
+import { BACK_IMG } from "../utils/constant";
 
 const GptSearch = () => {
     return (
         <div>
+            <div className="fixed top-0 left-0 w-full h-full -z-10">
+                <img
+                    src={BACK_IMG}
+                    alt="bg-image"
+                    className="w-full h-full object-cover"
+                />
+            </div>
             <GptSearchBar />
             <GptMovieSuggestion />
         </div>
